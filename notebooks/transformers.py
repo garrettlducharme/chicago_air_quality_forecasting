91828072d25f6486f063e9da1cc02d921ab5ca44
glducharme <garrettlducharme@gmail.com>
2020-03-10 20:51:52 -0500

    New function.

--- notebooks/transformers.py
+++ notebooks/transformers.py
@@ -7,3 +7,28 @@ def log_trans(y):
 def inv_log_trans(y):
     y = np.array(y)
     return 10**y - 1
+
+def df_shift(df, n_lags=1, n_obs=1):
+    """
+    Returns a dataframe with n_lags for all variables and n_obs for the sample value
+    """
+    n_vars = df.shape[1]
+    cols, col_names = [], []
+    # lags
+    for i in range(n_lags, 0, -1):
+        cols.append(df.shift(i))
+        col_names += [f'{col} (t-{i})' for col in df.columns]
+    # observations
+    for i in range(0, n_obs):
+        cols.append(df['Sample Value'].shift(-i))
+        if i == 0:
+            col_names.append('Sample Value (t)')
+        else:
+            col_names.append(f'Sample Value (t+{i})')
+            
+    shifted_df = pd.concat(cols, axis=1)
+    shifted_df.columns = col_names
+    shifted_df.dropna(inplace=True)
+    
+    return shifted_df
+
